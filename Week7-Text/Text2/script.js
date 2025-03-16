06d9e2f17e437e5a9b1ba55ae92b19b65bfcd65a
Helen Wu <wuz420@newschool.edu>
2025-03-16 17:23:42 -0400

    Updated Text#3//clicking to show random text from the order

--- Week7-Text/Text2/script.js
+++ Week7-Text/Text2/script.js
@@ -1,24 +1,81 @@
+// const storyGrammar = {
+//   start: "It was a dark and stormy night, and the $phrase1. $name, cursed with night blindness, $phrase2. The $noun3 grew closer and closer. Finally, $name was $verb2 by $phrase1 and the sky remained dark.",
+//   phrase1: "$object $verb3 $noun4 ",
+//   phrase2: "$adverb $verb1 the $object $noun3",
+//   subject: "$noun | $adjective $noun",
+//   object:
+//     "$adjective1 $noun1 like a $adjective2 $noun2",
+//   adverb: "exceptionally | somewhat",
+//   adjective1: " tenebrous | gloomy | bloodcurdling | horrific | arcane | wild",
+//   adjective2: " terrible | huge | striking | ghastly | awful ",
+//   noun1: "rock | wind | rain | light | fog ",
+//   noun2: "monster | banshee | cthulhu | ghost | zombie ",
+//   noun3: "footsteps | yells | rustling | knocking sounds | gurgling ",
+//   noun4: "windows on the buildings | trees on the streets | doors above the houses",
+//   verb1: "heard | saw | felt ",
+//   verb2: "attacked | eat | eroded |  murdered | stabbed | killed ",
+//   verb3: "lashed against | crushed | struck |  hit ",
+//   name: "Accolon | Arthur | Lancelot | Cador | Ector | Gareth"
+// };
+
+// const context = {};
+
+// const g = RiTa.grammar(storyGrammar, context);
+
+// display(g.expand());
+
+// function display(...strings) {
+//   const div = document.createElement("div");
+
+//   div.classList.add("text");
+
+//   div.innerText = strings.join("\n");
+//   document.body.append(div);
+// }
+
+
+// window.onscroll = function(e){
+//   if (window.scrollY > 3000) {
+
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = "red";
+
+//   } else {
+
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
+
+//   }
+// };
+
+
 const storyGrammar = {
   start: "It was a dark and stormy night, and the $phrase1. $name, cursed with night blindness, $phrase2. The $noun3 grew closer and closer. Finally, $name was $verb2 by $phrase1 and the sky remained dark.",
   phrase1: "$object $verb3 $noun4 ",
   phrase2: "$adverb $verb1 the $object $noun3",
   subject: "$noun | $adjective $noun",
-  object:
-    "$adjective1 $noun1 like a $adjective2 $noun2",
+  object: "$adjective1 $noun1 like a $adjective2 $noun2",
   adverb: "exceptionally | somewhat",
-  adjective1: " tenebrous | gloomy | bloodcurdling | horrific | arcane | wild",
-  adjective2: " terrible | huge | striking | ghastly | awful ",
-  noun1: "rock | wind | rain | light | fog ",
-  noun2: "monster | banshee | cthulhu | ghost | zombie ",
-  noun3: "footsteps | yells | rustling | knocking sounds | gurgling ",
+  adjective1: "tenebrous | gloomy | bloodcurdling | horrific | arcane | wild",
+  adjective2: "terrible | huge | striking | ghastly | awful",
+  noun1: "rock | wind | rain | light | fog",
+  noun2: "monster | banshee | cthulhu | ghost | zombie",
+  noun3: "footsteps | yells | rustling | knocking sounds | gurgling",
   noun4: "windows on the buildings | trees on the streets | doors above the houses",
-  verb1: "heard | saw | felt ",
-  verb2: "attacked | eat | eroded |  murdered | stabbed | killed ",
-  verb3: "lashed against | crushed | struck |  hit ",
-  name: "Accolon | Arthur | Lancelot | Cador | Ector | Gareth"
+  verb1: "heard | saw | felt",
+  verb2: "attacked | eaten | eroded | murdered | stabbed | killed",
+  verb3: "lashed against | crushed | struck | hit",
+  name: "$chosenName"
 };
 
-const context = {};
+
+const nameList = ["Accolon", "Arthur", "Lancelot", "Cador", "Ector", "Gareth"];
+const chosenName = nameList[Math.floor(Math.random() * nameList.length)];
+
+
+const context = {
+  chosenName: chosenName
+};
 
 const g = RiTa.grammar(storyGrammar, context);
 
@@ -26,25 +83,18 @@ display(g.expand());
 
 function display(...strings) {
   const div = document.createElement("div");
-
   div.classList.add("text");
-
-  // div.style = "font-size: 30px; margin: 10%; line-height: 1.5;";
   div.innerText = strings.join("\n");
   document.body.append(div);
 }
 
-
-window.onscroll = function(e){
+window.onscroll = function (e) {
   if (window.scrollY > 3000) {
-
     document.body.style.backgroundColor = "black";
     document.body.style.color = "red";
-
   } else {
-
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
-
   }
 };
+
---
+++ Week7-Text/Text3/script.js
@@ -0,0 +1,114 @@
+var firstText = [
+    "A",
+    "An",
+    "The",
+    "Her",
+    "His",
+    "Their",
+  ]
+  
+  var secondText = [
+    "star",
+    "stone",
+    "phone",
+    "cow",
+    "clock",
+    "film",
+    "speaker",
+    "rose",
+    
+  ]
+  
+  
+  var thirdText = [
+    "runs",
+    "walks",
+    "eats",
+    "writes",
+    "sings",
+    "sleeps",
+    "studies",
+    "investigates",
+    "has",
+  ]
+
+
+  
+  
+  
+  function addText() {
+    let firstWord = Math.floor(Math.random() * firstText.length);
+    let secondWord = Math.floor(Math.random() * secondText.length);
+    let thirdWord = Math.floor(Math.random() * thirdText.length);
+
+
+    function generateStoryParts() {
+        return [
+            firstText[Math.floor(Math.random() * firstText.length)], 
+            secondText[Math.floor(Math.random() * secondText.length)],
+            thirdText[Math.floor(Math.random() * thirdText.length)]
+        ];
+    }
+
+    let storyParts = generateStoryParts();
+
+    let clickCount = 0;
+
+    document.body.onclick = function () {
+        if (clickCount >= storyParts.length) {
+            storyParts = generateStoryParts(); 
+            clickCount = 0;
+        }
+
+
+        
+        let newDiv = document.createElement("div");
+
+document.onmousemove = function(e){
+    newDiv.style.position = "fixed";
+    newDiv.style.left = e.clientX + "px";
+    newDiv.style.top = e.clientY + "px";
+}
+
+        newDiv.classList.add("text");
+
+        newDiv.textContent = storyParts[clickCount];
+
+
+        document.body.appendChild(newDiv);
+
+        clickCount++;
+    };
+
+
+}
+
+addText();
+
+      
+
+
+// const newDiv = document.createElement("div");
+// newDiv.classList.add("text");
+// document.body.appendChild(newDiv);
+
+// let clickCount = 0;
+// const storyParts = ["start", "second", "third", "fourth"]; 
+
+// document.body.onclick = function(){
+
+//     let storyParts = ["start", "second", "third", "fourth"];
+
+//     if (clickCount >= storyParts.length) {
+//         clickCount = 0;
+//     }
+
+//     newDiv.innerText = g.expand(`$${storyParts[clickCount]}`);
+//     clickCount++;
+
+
+// }
+
+  
+
+
